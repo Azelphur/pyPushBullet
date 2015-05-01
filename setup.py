e4d0d82d6bb7259ea0f4525ddab3484702a7ec5f
Adam Baxter <voltagex@voltagex.org>
2015-05-01 21:58:45 +1000

    Set up for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pushbullet',
+      version='0.1',
+      description='A PushBullet API',
+      url='https://github.com/Azelphur/pyPushBullet',
+      author='Azelphur',
+      author_email='support@azelphur.com',
+      license='GPL',
+      packages=['pushbullet'],
+      zip_safe=False)
