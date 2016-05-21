789b5faa7fad6e5bdb09e791e28fe1068a7a0cf5
Lyude <thatslyude@gmail.com>
2016-05-21 13:00:25 -0400

    Clean trailing whitespace
    My vim setup highlights this red so it was bothering me :(

--- pushbullet.py
+++ pushbullet.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 # The MIT License (MIT)
+
 # Copyright (c) 2016 Alfie "Azelphur" Day
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -47,7 +48,7 @@ class Device(object):
     def __init__(self, pb, **kwargs):
         self.pb = pb
         self.attrs = kwargs
-        
+
 
     def __setitem__(self, key, value):
         if key in self.writable_attributes:
